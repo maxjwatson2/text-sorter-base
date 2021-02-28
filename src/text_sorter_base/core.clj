8272f775ddf21639e3b0fbbb7bca8bedc6a7d216
Max <max.watson@fidnip.com>
2021-02-28 11:13:57 -0800

    All sort functions added and tested. Most tests added. Everything apparently working

--- src/text_sorter_base/core.clj
+++ src/text_sorter_base/core.clj
@@ -1,26 +1,55 @@
 (ns text-sorter-base.core
   (require [clojure.java.io :as io]
            [clojure.string :as str]
+
            )
-  (:gen-class))
+  (:gen-class)
+  (:import (java.text SimpleDateFormat)))
 
-
+;; I think its wise to convert the date string to a proper date here because that's what we want to return. In a real situation it would depend on use case.
+;; It should be noted that these dates are down the the milisecond, which could be either a good thing or a bad thing depending on what kind of app we're making.
 (defn person-to-map [person-array]
-  {:last-name (person-array 0) :first-name (person-array 1) :email (person-array 2) :favorite-color (person-array 3) :birth-day (person-array 4)})
+  {:last-name (person-array 0) :first-name (person-array 1) :email (person-array 2) :favorite-color (person-array 3) :birth-date (.parse (SimpleDateFormat. "M/D/YYYY") (person-array 4))}) ;; NOTE this may break things so maybe dont
 
+;; A simple function to split the individual up by the delimiter(NOTE check if right word). Could be a problem is the delimiter is in the person's name but that's outside the scope of this assignment.
 (defn file-to-map [file]
   (let [parsed-blob (clojure.string/split-lines file)]
     (for [b parsed-blob]
       (do (cond
             (str/includes? file ",") (person-to-map (clojure.string/split b #","))
-            (str/includes? file "-") (person-to-map (clojure.string/split b #"-"));;NOTE replace this with |
+            (str/includes? file "-") (person-to-map (clojure.string/split b #""))
             (str/includes? file " ") (person-to-map (clojure.string/split b #" "))
             :else (println "File is invalid"))))))
+
+;; We only need email descending, birthdate ascending, and last name decending but I'm adding in a few more because it's simple, good for tests and would be usefull in a real business situation.
+(defn sort-by-last-name [person-map]
+  (reverse (sort-by :last-name person-map)))
+
+(defn sort-by-first-name [person-map]
+  (sort-by :first-name person-map))
+
+(defn sort-by-email [person-map]
+  (reverse (sort-by :email person-map)))
+
+(defn sort-by-color [person-map]
+  (sort-by :favorite-color person-map))
+
+(defn sort-by-date [person-map]
+  (->> person-map (sort-by last #(compare %1 %2))))
+
+(defn sort-map [person-map sort-method] ;; NOTE make sure that the right things are ascending/decending before finalization.
+  (case sort-method
+    "last name" (sort-by-last-name person-map)
+    "first name" (sort-by-first-name person-map)
+    "email" (sort-by-email person-map)
+    "color" (sort-by-color person-map)
+    "date" (sort-by-date person-map)
+    :else (println "Sort method invalid")))
 
 (defn -main
   "Currently pulls a file form location and sorts it into a map."
   [file-location]
   (let [mapped-file (file-to-map(slurp (io/resource file-location)))]
+    (sort-map mapped-file "last name")
     mapped-file
-    )
-  )
+    ))
--- test/text_sorter_base/core_test.clj
+++ test/text_sorter_base/core_test.clj
@@ -1,19 +1,43 @@
 (ns text-sorter-base.core-test
   (:require [clojure.test :refer :all]
+            [clojure.java.io :as io]
             [text-sorter-base.core :refer :all]
-            ))
-
-(deftest main-tests
-  (testing "Sanity"
-    (is (= 1 1)))
-  (testing "main functionality"
-    (println (-main "csvnames.txt"))
-    (is (= 2 (count (-main "csvnames.txt"))))
-    (is (= 5 (count (first(-main "csvnames.txt")))))
-    (is (= "Jack" (:first-name (first(-main "csvnames.txt")))))
-
-    )
-
+            )
+  (:import (java.text SimpleDateFormat))
   )
 
+(deftest main-tests
+  (testing "Sanity";; Basic test for sanity
+    (is (= 1 1)))
+  (testing "main functionality" ;; Tests the top level funcitons of the app.
+    (is (= 3 (count (-main "csvnames.txt"))))
+    (is (= 5 (count (first(-main "csvnames.txt")))))
+    (is (= "Jack" (:first-name (first(-main "csvnames.txt")))))
+    (is (= 3 (count (file-to-map(slurp (io/resource "csvnames.txt"))))))
+    )
 
+  ;; Here's a test map. The reason we don't just use the file-to-map funciton is that if that one funciton breaks all these tests will fail.
+  ;; This makes it way easier to narrow down points of failure.
+  (testing "sorting functions"
+    (let[sorting-name [
+                       {:last-name "Smith" :first-name "Adam" :email "as@fake.com" :favorite-color "green" :birth-date  (.parse (SimpleDateFormat. "M/D/YYYY")"2/12/1709")}
+                       {:last-name "Lincon" :first-name "Abe" :email "al@fake.com" :favorite-color "brown" :birth-date  (.parse (SimpleDateFormat. "M/D/YYYY")"2/12/1809")}
+                       {:last-name "Teach" :first-name "Edward" :email "et@fake.com" :favorite-color "black" :birth-date  (.parse (SimpleDateFormat. "M/D/YYYY")"1/1/1619")}
+                       {:last-name "Stede" :first-name "Bonnet" :email "sb@fake.com" :favorite-color "yellow" :birth-date  (.parse (SimpleDateFormat. "M/D/YYYY")"1/1/1629")}
+                       ]]
+      ;; here we're testing the sorting
+      (is (= "Lincon" (:last-name (last (sort-by-last-name sorting-name)))))
+      (is (= "Teach" (:last-name (first (sort-by-last-name sorting-name)))))
+
+      (is (= "Abe" (:first-name (first (sort-by-first-name sorting-name)))))
+      (is (= "Edward" (:first-name (last (sort-by-first-name sorting-name)))))
+
+      (is (= "al@fake.com" (:email (last (sort-by-email sorting-name)))))
+      (is (= "sb@fake.com" (:email (first (sort-by-email sorting-name)))))
+
+      (is (= "black" (:favorite-color (first (sort-by-color sorting-name)))))
+      (is (= "yellow" (:favorite-color (last (sort-by-color sorting-name)))))
+
+      (is (= "Edward" (:first-name (first (sort-by-date sorting-name)))))
+      (is  (= "Abe" (:first-name (last (sort-by-date sorting-name)))))
+      )))
